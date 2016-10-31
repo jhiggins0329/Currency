e241d96bc70f54fc7b86bb2ed2f7699a2e0e5f0e
James Higgins <jhiggins0329@gmail.com>
2016-10-31 16:49:35 -0400

    edited file

---
+++ currency.py
@@ -0,0 +1,12 @@
+class Currency():
+    def currency(self, currency_code, value)
+    self.currrency_code = currency_code
+    self.value = value
+    return
+
+    def (arg):
+        pass __add__(self, other):
+        if self.value == other.value
+            return Currency
+
+    def hg
